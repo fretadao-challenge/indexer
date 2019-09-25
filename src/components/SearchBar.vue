7ae7007914bbd679ab0b3d53178052ea75629d54
Alax Alves <alaxallves@gmail.com>
2019-09-25 15:13:17 -0300

    Searching Profile from API and listing it

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,42 @@
 <template>
-  <q-input v-model="search" filled type="search" hint="Search">
-      <q-icon name="search"/>
+  <q-input v-model="query"
+           @keyup.enter="search()"
+           ref="search"
+           filled
+           type="search"
+           hint="Search">
+    <q-icon name="search"/>
   </q-input>
 </template>
 
 <script>
+import { HTTP } from '../boot/axios.js';
+
 export default {
   name: 'SearchBar',
+  data() {
+    return {
+      query: '',
+    };
+  },
+  methods: {
+    search() {
+      HTTP.get('search', {
+        params: {
+          query: this.query,
+        },
+      })
+        .then((response) => {
+          const { data } = response;
+          this.$store.dispatch('searchResults', { searchResults: data });
+          this.$router.push({ name: 'ProfileList' });
+          console.log(this.$router);
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
 };
 </script>
 
--- src/pages/ProfileList.vue
+++ src/pages/ProfileList.vue
@@ -1,15 +1,15 @@
 <template>
   <q-page class="flex flex-center">
     <h1>Search Results</h1>
-    <q-list>
+    <q-list v-for="item in searchResults" :key="item.username">
       <q-item clickable>
         <q-item-section avatar>
           <q-avatar color="primary" text-color="white">
-            M
+            {{item.name[0].toUpperCase()}}
           </q-avatar>
         </q-item-section>
         <q-item-section>
-          <q-item-label>Member name</q-item-label>
+          <q-item-label>{{ item.name }}</q-item-label>
           <q-item-label caption>Shortened URL</q-item-label>
         </q-item-section>
 
@@ -23,9 +23,15 @@
 </template>
 
 <script>
+import { mapState } from 'vuex';
 
 export default {
   name: 'ProfileList',
+  computed: {
+    ...mapState({
+      searchResults: state => state.search.searchResults,
+    }),
+  },
 };
 </script>
 
