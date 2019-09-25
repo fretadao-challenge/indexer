955d3c73e9b3057436f361f685a7a8dba0c9ae32
Alax Alves <alaxallves@gmail.com>
2019-09-25 17:50:19 -0300

    Creating additional mixin functions

--- src/mixins/helper.js
+++ src/mixins/helper.js
@@ -1,9 +1,34 @@
-const goToProfileMixin = {
+import { HTTP } from '../boot/axios.js';
+
+const Mixin = {
   methods: {
     goToProfile(profile) {
       this.$router.push({ name: 'Profile', params: { id: profile.id } });
     },
+    goToProfileEdit() {
+      this.$router.push({ name: 'ProfileEdit' });
+    },
+    deleteProfile() {
+      HTTP.delete(`profiles/${this.$route.params.id}`)
+        .then(() => {
+          this.$router.push({ name: 'Home' });
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+    getProfile() {
+      HTTP.get(`profiles/${this.$route.params.id}`)
+        .then((response) => {
+          const { data } = response;
+          this.name = data.name;
+          this.twitter_url = data.twitter_url;
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
   },
 };
 
-export default goToProfileMixin;
+export default Mixin;
